39e88140bb9b5b7dbedb97a249eb721fc4fb95ad
dorpmueller <philip.dorpmueller@tu-dortmund.de>
2025-06-08 08:12:52 +0200

    tests: load data from paypal export

--- test/testgnucashConverter/testLoadData.py
+++ test/testgnucashConverter/testLoadData.py
@@ -22,5 +22,23 @@ class TestLoaderBarclays(unittest.TestCase):
         self.assertEqual(self._loader.data[1].Description, "Test2")
 
 
+class TestLoaderPaypal(unittest.TestCase):
+    def setUp(self) -> None:
+        self._loader = ldb.DataLoaderPaypal("test/data/paypal.xlsx")
+
+    def testLoad(self):
+        """
+        Test the load method of DataLoaderXlsx.
+        """
+        self._loader.load()
+
+        self.assertEqual(self._loader.data[0].Date, "08.06.2024")
+        self.assertEqual(self._loader.data[0].Deposit, -72.9)
+        self.assertEqual(self._loader.data[0].Description, "Von Nutzer eingeleitete Abbuchung")
+
+        self.assertEqual(self._loader.data[1].Date, "21.06.2024")
+        self.assertEqual(self._loader.data[1].Deposit, -27.5)
+        self.assertEqual(self._loader.data[1].Description, "Handyzahlung")
+
 if __name__ == "__main__":
     unittest.main()
