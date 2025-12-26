810c915f01fc9fac8a90d03c692f486461d8f540
Philip Dorpmüller <philip.dorpmueller@web.de>
2025-12-26 20:53:49 +0100

    feat: cli transaction transfer to firefly

--- src/gnucashConverter/__scripts__.py
+++ src/gnucashConverter/__scripts__.py
@@ -3,6 +3,7 @@ import toml
 
 from gnucashConverter import convertData as cdt
 from gnucashConverter import loadData as ldb
+from gnucashConverter import fireflyInterface as ffi
 
 
 def convert():
@@ -35,5 +36,41 @@ def convert():
         converter = cdt.ConvertData(loader.load(), accountMap)
         converter.assignAccounts()
         converter.saveCsv(arguments.output_file)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def transfer():
+    parser = argparse.ArgumentParser("transaction transfer")
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Path to the input file to be converted.",
+    )
+    parser.add_argument(
+        "--interface_config",
+        type=str,
+        help="Path to the interface configuration file.",
+        required=True,
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        choices=["barclays", "paypal", "trade_republic"],
+        required=True,
+    )
+    parser.add_argument("--account_name", type=str, help="Name of the account to assign to loaded transactions.", default=None)
+    arguments = parser.parse_args()
+
+    loader: ldb.DataLoader = ldb.loaderMapping[arguments.source](arguments.input_file, arguments.account_name)
+    loader.load()
+
+    try:
+        interfaceConfig = toml.load(arguments.interface_config)
+        interface = ffi.FireflyInterface(**interfaceConfig)
+
+        for transaction in loader.transactions:
+            response = interface.createTransaction(transaction)
+            print(f"Processed transaction with response: {response.status_code}")
+
     except Exception as e:
         print(f"An error occurred: {e}")
