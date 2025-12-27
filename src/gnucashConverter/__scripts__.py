49ec1cab64ab933834372f78153d2d6544c417de
Philip Dorpmüller <philip.dorpmueller@web.de>
2025-12-27 12:49:48 +0100

    feat: import transactions to firefly (#6)

---
+++ src/gnucashConverter/__scripts__.py
@@ -0,0 +1,71 @@
+import argparse
+import toml
+
+from gnucashConverter import convertData as cdt
+from gnucashConverter import loadData as ldb
+from gnucashConverter import fireflyInterface as ffi
+
+
+def convert():
+    parser = argparse.ArgumentParser("data converter")
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Path to the input file to be converted.",
+    )
+    parser.add_argument(
+        "output_file",
+        type=str,
+        help="Path to the output file where the converted data will be saved.",
+    )
+    parser.add_argument("--account_map", type=str, help="Path to the account map definitions.", default=None)
+    parser.add_argument(
+        "--source",
+        type=str,
+        choices=["barclays", "paypal", "trade_republic", "common"],
+        required=True,
+    )
+    parser.add_argument("--account_name", type=str, help="Name of the account to assign to loaded transactions.", default=None)
+
+    arguments = parser.parse_args()
+
+    loader = ldb.loaderMapping[arguments.source](arguments.input_file, arguments.account_name)
+    transactions = loader.load()
+
+    accountMap = toml.load(arguments.account_map) if arguments.account_map else None
+    converter = cdt.ConvertData(transactions, accountMap)
+    converter.assignAccounts()
+    converter.saveCsv(arguments.output_file)
+
+def transfer():
+    parser = argparse.ArgumentParser("transaction transfer")
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Path to the input file to be converted.",
+    )
+    parser.add_argument(
+        "--interface_config",
+        type=str,
+        help="Path to the interface configuration file.",
+        required=True,
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        choices=["barclays", "paypal", "trade_republic", "common"],
+        required=True,
+    )
+    parser.add_argument("--account_name", type=str, help="Name of the account to assign to loaded transactions.", default=None)
+    arguments = parser.parse_args()
+
+    loader = ldb.loaderMapping[arguments.source](arguments.input_file, arguments.account_name)
+    transactions = loader.load()
+
+    interfaceConfig = toml.load(arguments.interface_config)
+    interface = ffi.FireflyInterface(**interfaceConfig)
+
+    for transaction in transactions:
+        response = interface.createTransaction(transaction)
+        print(f"Processed transaction with response: {response.status_code}")
+
--- test/testgnucashConverter/testLoadData.py
+++ test/testgnucashConverter/testLoadData.py
@@ -1,69 +1,94 @@
 import unittest
 
 from gnucashConverter import loadData as ldb
+from gnucashConverter import data
 
 
 class TestLoaderBarclays(unittest.TestCase):
     def setUp(self) -> None:
-        self._loader = ldb.DataLoaderBarclays("test/data/barclays.xlsx")
+        self._loader = ldb.DataLoaderBarclays("test/data/barclays.xlsx", "Barclays")
 
     def testLoad(self):
         """
         Test the load method of DataLoaderTr.
         """
-        self._loader.load()
+        transactions = self._loader.load()
 
-        self.assertEqual(self._loader.transactions[0].Date, "2025-05-30")
-        self.assertEqual(self._loader.transactions[0].Deposit, -1619.25)
-        self.assertEqual(self._loader.transactions[0].Description, "Test1 - Test2Händler")
+        self.assertEqual(transactions[0].date, "2025-05-30")
+        self.assertEqual(transactions[0].amount, 1619.25)
+        self.assertEqual(transactions[0].description, "Test1 - Test2Händler")
+        self.assertEqual(transactions[0].source_name, "Barclays")
+        self.assertEqual(transactions[0].type, data.TransactionType.WITHDRAWAL.value)
 
-        self.assertEqual(self._loader.transactions[1].Date, "2024-05-30")
-        self.assertEqual(self._loader.transactions[1].Deposit, -13.32)
-        self.assertEqual(self._loader.transactions[1].Description, "Test2 - Test1h")
+        self.assertEqual(transactions[1].date, "2024-05-30")
+        self.assertEqual(transactions[1].amount, 13.32)
+        self.assertEqual(transactions[1].description, "Test2 - Test1h")
+        self.assertEqual(transactions[1].source_name, "Barclays")
+        self.assertEqual(transactions[1].type, data.TransactionType.WITHDRAWAL.value)
+
 
 class TestLoaderPaypal(unittest.TestCase):
     def setUp(self) -> None:
-        self._loader = ldb.DataLoaderPaypal("test/data/paypal.csv")
+        self._loader = ldb.DataLoaderPaypal("test/data/paypal.csv", "Paypal")
 
     def testLoad(self):
         """
         Test the load method of DataLoaderPaypal.
         """
-        self._loader.load()
+        transactions = self._loader.load()
 
-        self.assertEqual(self._loader.transactions[0].Date, "2025-07-04")
-        self.assertEqual(self._loader.transactions[0].Deposit, 60.0)
-        self.assertEqual(self._loader.transactions[0].Description, "Handyzahlung; asdf - rf@gmx.net - asdf")
+        self.assertEqual(transactions[0].date, "2025-07-04")
+        self.assertEqual(transactions[0].amount, 60.0)
+        self.assertEqual(transactions[0].description, "Handyzahlung; asdf - rf@gmx.net - asdf")
+        self.assertEqual(transactions[0].destination_name, "Paypal")
+        self.assertEqual(transactions[0].type, data.TransactionType.DEPOSIT.value)
 
-        self.assertEqual(self._loader.transactions[1].Date, "2025-07-04")
-        self.assertEqual(self._loader.transactions[1].Deposit, -3.0)
-        self.assertEqual(self._loader.transactions[1].Description, "Handyzahlung - pbfd")
+        self.assertEqual(transactions[1].date, "2025-07-04")
+        self.assertEqual(transactions[1].amount, 3.0)
+        self.assertEqual(transactions[1].description, "Handyzahlung - pbfd")
+        self.assertEqual(transactions[1].source_name, "Paypal")
+        self.assertEqual(transactions[1].type, data.TransactionType.WITHDRAWAL.value)
 
 
 class TestLoaderTr(unittest.TestCase):
     def setUp(self) -> None:
-        self._loader = ldb.DataLoaderTr("test/data/trade_republic.csv")
+        self._loader = ldb.DataLoaderTr("test/data/trade_republic.csv", "tr")
 
     def testLoad(self):
         """
         Test the load method of DataLoaderTr.
         """
-        self._loader.load()
+        transactions = self._loader.load()
 
-        self.assertEqual(self._loader.transactions[0].Date, "2024-02-06")
-        self.assertEqual(self._loader.transactions[0].Deposit, 10000.0)
-        self.assertEqual(self._loader.transactions[0].Description, "asdf - Deposit")
+        self.assertEqual(transactions[0].date, "2024-02-06T15:46:07")
+        self.assertEqual(transactions[0].amount, 10000.0)
+        self.assertEqual(transactions[0].description, "asdf - Deposit")
+        self.assertEqual(transactions[0].destination_name, "tr")
+        self.assertEqual(transactions[0].type, data.TransactionType.DEPOSIT.value)
 
-        self.assertEqual(self._loader.transactions[1].Date, "2025-07-01")
-        self.assertEqual(self._loader.transactions[1].Deposit, 48.24)
-        self.assertEqual(self._loader.transactions[1].Description, "ijkl - Interest")
-        self.assertEqual(self._loader.transactions[2].Date, "2025-07-02")
-        self.assertEqual(self._loader.transactions[2].Deposit, 128.74)
-        self.assertEqual(self._loader.transactions[2].Description, "korrekt - Tax Refund")
+        self.assertEqual(transactions[1].date, "2025-07-01T05:22:12")
+        self.assertEqual(transactions[1].amount, 48.24)
+        self.assertEqual(transactions[1].description, "ijkl - Interest")
+        self.assertEqual(transactions[1].destination_name, "tr")
+        self.assertEqual(transactions[1].type, data.TransactionType.DEPOSIT.value)
 
-        self.assertEqual(self._loader.transactions[3].Date, "2025-08-01")
-        self.assertEqual(self._loader.transactions[3].Deposit, -115.0)
-        self.assertEqual(self._loader.transactions[3].Description, "money - Removal")
+        self.assertEqual(transactions[2].date, "2025-07-02T00:41:26")
+        self.assertEqual(transactions[2].amount, 128.74)
+        self.assertEqual(transactions[2].description, "korrekt - Tax Refund")
+        self.assertEqual(transactions[2].destination_name, "tr")
+        self.assertEqual(transactions[2].type, data.TransactionType.DEPOSIT.value)
+
+        self.assertEqual(transactions[3].date, "2025-08-01T12:14:31")
+        self.assertEqual(transactions[3].amount, 115.0)
+        self.assertEqual(transactions[3].description, "money - Removal")
+        self.assertEqual(transactions[3].source_name, "tr")
+        self.assertEqual(transactions[3].type, data.TransactionType.WITHDRAWAL.value)
+
+
+class TestLoaderCommon(TestLoaderTr):
+    def setUp(self) -> None:
+        self._loader = ldb.DataLoaderCommon("test/data/common.csv")
+
 
 if __name__ == "__main__":
     unittest.main()
