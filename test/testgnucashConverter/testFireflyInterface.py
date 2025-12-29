a57562fb79c06f256ce94caa78e7a4bb20a3f989
Philip Dorpmüller <philip.dorpmueller@web.de>
2025-12-29 18:04:42 +0100

    tests: creation of transactions

--- test/testgnucashConverter/testFireflyInterface.py
+++ test/testgnucashConverter/testFireflyInterface.py
@@ -1,15 +1,14 @@
 import os
 import unittest
 from typing import Set
-from unittest.mock import Mock
 
 from gnucashConverter import data
 from gnucashConverter import fireflyInterface as ffi
 from gnucashConverter import loadData as ldb
 
 
-class TestAccountInterface(unittest.TestCase):
-    def setUp(self) -> None:
+class TestInterfaceBase(unittest.TestCase):
+    def setUp(self):
         api_token = os.getenv("API_TOKEN")
         assert api_token is not None, "API_TOKEN environment variable must be set for tests."
 
@@ -17,12 +16,14 @@ class TestAccountInterface(unittest.TestCase):
             base_url="http://localhost",
             api_token=api_token,
         )
+        self.addCleanup(self._removeFireflyData)
         self._transactions = ldb.DataLoaderCommon("test/data/common").load()
 
-    def tearDown(self) -> ldb.NoneType:
+    def _removeFireflyData(self):
         self._fireflyInterface.deleteAccounts()
+        self._fireflyInterface.purgeUserData()
 
-    def testCreateDeleteAccounts(self):
+    def _getAccountNames(self) -> Set[str]:
         account_names: Set[str] = set()
 
         for transaction in self._transactions:
@@ -30,6 +31,13 @@ class TestAccountInterface(unittest.TestCase):
                 account_names.add(transaction.source_name)
             if transaction.destination_name is not None:
                 account_names.add(transaction.destination_name)
+
+        return account_names
+
+
+class TestAccountInterface(TestInterfaceBase):
+    def testCreateDeleteAccounts(self):
+        account_names = self._getAccountNames()
 
         for account_name in account_names:
             self._fireflyInterface.createAccount(data.PostAssetAccount(account_name))
@@ -43,8 +51,24 @@ class TestAccountInterface(unittest.TestCase):
         self.assertEqual(len(self._fireflyInterface.getAccounts()), 0, "Accounts were not deleted from the server.")
 
 
-class TestTransactionInterface(unittest.TestCase):
-    pass
+class TestTransactionInterface(TestInterfaceBase):
+    def setUp(self):
+        super().setUp()
+
+        for account_name in self._getAccountNames():
+            self._fireflyInterface.createAccount(data.PostAssetAccount(account_name))
+
+    def testCreateTransactions(self):
+        for transaction in self._transactions:
+            self._fireflyInterface.createTransaction(transaction)
+
+        server_transactions = self._fireflyInterface.getTransactions(limit=100, page=1)
+        self.assertEqual(
+            len(server_transactions),
+            len(self._transactions),
+            "Number of transactions on the server does not match the number of created transactions.",
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
